ed7a0ecf3337a25be77f2e353a7f21b938142dbd
FairyZeta <fairyzeta@kimamani.jp>
2016-04-08 14:12:29 +0900

    ソースファイル欠損の補修

---
+++ FairyZeta.FF14.ACT.Timeline.Core/Data/OverlayOptionData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyZeta.FF14.ACT.Timeline.Core.Data
+{
+    /// <summary> タイムライン／オーバーレイオプションデータ
+    /// </summary>
+    public class OverlayOptionData : _Data
+    {
+      /*--- Property/Field Definitions ------------------------------------------------------------------------------------------------------------------------------*/
+
+
+      /*--- Constructers --------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> タイムライン／オーバーレイオプションデータ／コンストラクタ
+        /// </summary>
+        public OverlayOptionData()
+        {
+            this.initData();
+            this.clear();
+        }
+
+      /*--- Method: Initialization ----------------------------------------------------------------------------------------------------------------------------------*/
+        
+        /// <summary> データの初期化を実行します。
+        /// </summary>
+        /// <returns> 正常終了時 True </returns> 
+        private bool initData()
+        {
+            return true;
+        }
+
+      /*--- Method: public ------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> データの全体クリアを実行します。
+        /// </summary>
+        /// <returns> 正常終了時 True </returns> 
+        public override bool Clear()
+        {
+            base.Clear();
+            this.clear();
+
+            return true;
+        }
+
+      /*--- Method: private -----------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> データの単体クリアを実行します。
+        /// </summary>
+        /// <returns> 正常終了時 True </returns> 
+        private bool clear()
+        {
+            return true;
+        }
+    }
+}
---
+++ FairyZeta.FF14.ACT.Timeline.Core/WPF/Desing/Desing_OverlayCustomWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FairyZeta.FF14.ACT.Timeline.Core.WPF.ViewModels;
+using FairyZeta.FF14.ACT.Timeline.Core.Data;
+
+namespace FairyZeta.FF14.ACT.Timeline.Core.WPF.Desing
+{
+    /// <summary> [画面デザイン用] タイムライン／オーバーレイカスタムウィンドウビューモデル
+    /// </summary>
+    public class Desing_OverlayCustomWindowViewModel : OverlayCustomWindowViewModel
+    {
+        /*--- Property/Field Definitions ------------------------------------------------------------------------------------------------------------------------------*/
+
+        /*--- Constructers --------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> [画面デザイン用] タイムライン／オーバーレイカスタムウィンドウビューモデル／コンストラクタ
+        /// </summary>
+        public Desing_OverlayCustomWindowViewModel()
+            : base()
+        {
+            this.initViewModel();
+            this.createDesingData_P001();
+        }
+
+        /*--- Method: Initialization ----------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> ビューモデルの初期化を実行します。
+        /// </summary>
+        /// <returns> 正常終了時 True </returns> 
+        private bool initViewModel()
+        {
+            return true;
+        }
+
+        /*--- Method: public ------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /*--- Method: private -----------------------------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> デザイン用データ（パターン１）の生成
+        /// </summary>
+        private void createDesingData_P001()
+        {
+
+        }
+    }
+}
